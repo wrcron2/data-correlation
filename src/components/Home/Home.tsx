e448154b7404c26d2200906bd329f130685d9d6f
ronoolo <ron.leibovitch@oolo.io>
2024-04-12 21:36:51 +0300

    change style

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -16,50 +16,7 @@ import Button from "../Button/Button.tsx";
 import { Overview } from "../Overview/Overview.tsx";
 import App1 from "../../services/App1.ts";
 import { Metrics } from "../../App.tsx";
-const dataSOV = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+
 
 
 const metricISname = "metric"
@@ -181,7 +138,7 @@ export const Home:React.FC<HomeProps> = ({ apps, selected }) => {
       </div>
       <hr className="hr-horizantal"></hr>
         <div className="graph-section">
-          <ResponsiveContainer width="100%" height="100%">
+          <ResponsiveContainer width="100%" height="98%">
             {renderSelectedGraph()}
       
         </ResponsiveContainer>
